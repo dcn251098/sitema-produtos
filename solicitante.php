f9292a97290776c5a535f811cb7f230ce683c2f3
dcn251098 <57157934+dcn251098@users.noreply.github.com>
2023-05-03 14:40:19 -0300

    Add files via upload

--- solicitante.php
+++ solicitante.php
@@ -93,39 +93,67 @@
     <div class="box">
         <form action="solicitante.php">
             <fieldset>
-                <legend><b>Fazer Pedido</b></legend>
+                <legend><b>Dados do Solicitante</b></legend>
                 <br>
                 <div class="inputBox">
                     <input type="text" name="id" id="id" class="inputUser" required>
-                    <label for="id" class="labelInput">Código do Pedido</label>
+                    <label for="id" class="labelInput">Solicitante</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
                     <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label for="nome" class="labelInput">Nome do Produto</label>
-                </div>
-                <br><br>
-                <label for="data_pedido"><b>Data do Pedido:</b></label>
-                <input type="date" name="data_pedido" id="data_pedido" required>
-                <br><br>
-                <div class="inputBox">
-                    <input type="number" name="quantidade" id="quantidade" class="inputUser" required>
-                    <label for="quantidade" class="labelInput">Quantidade</label>
+                    <label for="nome" class="labelInput">CEP</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="text" name="valor" id="valor" class="inputUser" required>
-                    <label for="valor" class="labelInput">Valor Unitário</label>
+                    <label for="data_toggle"><b>Estado:</b></label>
+                        <td align="left">
+                            <select name="estado"> 
+                                <option value="--">----</option>
+                                <option value="ac">Acre</option> 
+                                <option value="al">Alagoas</option> 
+                                <option value="am">Amazonas</option> 
+                                <option value="ap">Amapá</option> 
+                                <option value="ba">Bahia</option> 
+                                <option value="ce">Ceará</option> 
+                                <option value="df">Distrito Federal</option> 
+                                <option value="es">Espírito Santo</option> 
+                                <option value="go">Goiás</option> 
+                                <option value="ma">Maranhão</option> 
+                                <option value="mt">Mato Grosso</option> 
+                                <option value="ms">Mato Grosso do Sul</option> 
+                                <option value="mg">Minas Gerais</option> 
+                                <option value="pa">Pará</option> 
+                                <option value="pb">Paraíba</option> 
+                                <option value="pr">Paraná</option> 
+                                <option value="pe">Pernambuco</option> 
+                                <option value="pi">Piauí</option> 
+                                <option value="rj">Rio de Janeiro</option> 
+                                <option value="rn">Rio Grande do Norte</option> 
+                                <option value="ro">Rondônia</option> 
+                                <option value="rs">Rio Grande do Sul</option> 
+                                <option value="rr">Roraima</option> 
+                                <option value="sc">Santa Catarina</option> 
+                                <option value="se">Sergipe</option> 
+                                <option value="sp">São Paulo</option> 
+                                <option value="to">Tocantins</option> 
+                            </select>
+                        </td>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label for="nome" class="labelInput">Solicitante</label>
+                    <input type="text" name="minicipio" id="minicipio" class="inputUser" required>
+                    <label for="minicipio" class="labelInput">Município</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label for="nome" class="labelInput">Despachante</label>
+                    <input type="text" name="rua" id="rua" class="inputUser" required>
+                    <label for="rua" class="labelInput">Rua</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="numero" id="numero" class="inputUser" required>
+                    <label for="numero" class="labelInput">Número</label>
                 </div>
                 <br><br>
                 <input type="submit" name="submit" id="submit">
